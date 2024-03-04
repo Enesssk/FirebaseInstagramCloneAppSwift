2b466aa48514f60b932238c12d258c5c714489e4
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-03-04 18:12:02 +0300

    storage and firestore are enabled

--- FirebaseInstagramCloneAppSwift/UploadViewController.swift
+++ FirebaseInstagramCloneAppSwift/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class UploadViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
@@ -42,6 +44,70 @@ class UploadViewController: UIViewController,UIImagePickerControllerDelegate,UIN
     
 
     @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        //image ı dataya çeviriyoruz ki kaydedelim.
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpeg")
+            
+            imageReference.putData(data, metadata: nil) { metaData, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "error", messageInput: error!.localizedDescription)
+                } else{
+                    
+                    imageReference.downloadURL { url, error in
+                        
+                        
+                        if error != nil {
+                            let imageUrl = url?.absoluteString
+                            
+                            
+                            //save to Database
+                            
+                            let firestoreDatabase = Firestore.firestore()
+                            var firestoreReference : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser?.email!, "comment" : self.commentText.text!, "date" : FieldValue.serverTimestamp(), "like" : 0] as [String : Any]
+                            
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { error in
+                                
+                                if error != nil {
+                                    self.makeAlert(titleInput: "error", messageInput: error?.localizedDescription ?? "ERROR")
+                                } else {
+                                    self.tabBarController?.selectedIndex = 0
+                                }
+                    
+                            })
+                            
+                            
+                        }
+                        
+                        
+                        
+                        
+                    }
+                    
+                }
+            }
+           
+            
+        }
+        
+        
+    }
+    
+    func makeAlert (titleInput : String, messageInput : String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 }
